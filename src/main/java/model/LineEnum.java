d4ae3949431f22651f7be64bcb1443de79bfc756
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-03-19 02:23:04 -0300

    first commit with back-end files

---
+++ src/main/java/model/LineEnum.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum LineEnum {
+	CRONOS("Cronos"),
+	ARES(  "Ares");
+
+	private final String name;
+
+	LineEnum(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
