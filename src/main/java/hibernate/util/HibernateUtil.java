e7d24ee12c7cb4e9a581d133444d61ebd3de4dab
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-04-04 09:58:13 -0300

    Changed method catch to "Throwable" to print if exists fail in initial creation SessionFactory

--- src/main/java/hibernate/util/HibernateUtil.java
+++ src/main/java/hibernate/util/HibernateUtil.java
@@ -8,13 +8,13 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            //Create the session factory object from config.xml.
+            // Create the SessionFactory from hibernate.cfg.xml
             return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sessionFactory;
     }
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
