9f14fa330dfd412f228be9f4b3eaffc0915c773f
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-05-04 15:22:40 -0300

    Singleton implemented in back-end

---
+++ src/main/java/helper/DataBaseHelper.java
@@ -0,0 +1,17 @@
+package helper;
+
+import org.hibernate.Session;
+import util.HibernateUtil;
+
+public class DataBaseHelper {
+
+    public DataBaseHelper() {}
+    private static Session session;
+
+    public static Session getInstance() {
+        if (session == null) {
+            session = HibernateUtil.getSessionFactory().openSession();
+        }
+        return session;
+    }
+}
